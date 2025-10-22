d0972906a6ff19d98968f5c201449e9a9a1fd5b4
tangguodong <tangguodong@baidu.com>
2025-10-22 15:19:11 +0800

    add antd

--- demos/App.tsx
+++ demos/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import '@ant-design/v5-patch-for-react-19';
+import {ConfigProvider, Button} from 'antd';
 
 const App = () => {
   return (
-    <div>123</div>
+    <ConfigProvider>
+      <Button type="primary">123</Button>
+    </ConfigProvider>
   );
 };
 
