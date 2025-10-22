3dc59b98881ce048e2486e9591f9af6b18aab0aa
tangguodong <tangguodong@baidu.com>
2025-10-22 15:40:01 +0800

    demos: layout

--- demos/App.tsx
+++ demos/App.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import {lazy} from 'react';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import '@ant-design/v5-patch-for-react-19';
-import {ConfigProvider, Button} from 'antd';
+import {ConfigProvider, Layout} from 'antd';
+import './index.css';
+
+const Home = lazy(() => import('./src/Home'));
+const Test = lazy(() => import('./src/Test'));
+
+const {Sider, Content} = Layout;
+
+const layoutRootStyle: React.CSSProperties = {
+    height: '100%',
+    backgroundColor: '#ffffff',
+};
+
+const siderStyle: React.CSSProperties = {
+    height: '100%',
+    backgroundColor: '#ffffff',
+    padding: '16px',
+};
+
+const contentStyle: React.CSSProperties = {
+    height: '100%',
+    overflow: 'hidden',
+    overflowY: 'auto',
+    padding: '16px',
+};
 
 const App = () => {
   return (
     <ConfigProvider>
-      <Button type="primary">123</Button>
+      <BrowserRouter>
+        <Layout style={layoutRootStyle}>
+          <Sider width={200} style={siderStyle}>
+            sider
+          </Sider>
+          <Layout style={contentStyle}>
+            <Content>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/test" element={<Test />} />
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+            </Content>
+          </Layout>
+        </Layout>
+      </BrowserRouter>
     </ConfigProvider>
   );
 };
---
+++ demos/src/Test/index.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <div>test</div>
+    );
+};
+
+export default Page;
